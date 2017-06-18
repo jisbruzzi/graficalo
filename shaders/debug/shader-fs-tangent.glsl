d3147364e0c66ff49e28a6088b373a15119dd27c
soyyo5159 <soyyo5159@hotmail.com>
2017-06-18 17:48:17 -0300

    Agrego los shader para que debugees y la interfaz para normal y binormal

--- shaders/debug/shader-fs-tangent.glsl
+++ shaders/debug/shader-fs-tangent.glsl
@@ -4,4 +4,13 @@ varying vec3 vVertexTangent;
 
 void main(void) {
   gl_FragColor = vec4(vVertexTangent, 1.0);
+  if (gl_FragColor.x==-1.0){
+    gl_FragColor.x=0.5;
+  }
+  if (gl_FragColor.y==-1.0){
+    gl_FragColor.y=0.5;
+  }
+  if (gl_FragColor.z==-1.0){
+    gl_FragColor.z=0.5;
+  }
 }
