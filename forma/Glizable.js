15343acfee48dd0db22cb514bf986c18f54632a8
soyyo5159 <soyyo5159@hotmail.com>
2017-06-18 20:28:43 -0300

    Refactorizo la glizacion para la optimizacion

--- forma/Glizable.js
+++ forma/Glizable.js
@@ -1,5 +1,5 @@
 function Glizable(f,gl){
-  /*
+
   function TipoBuffer(nombreComun,fabrica,nombreEnShader){
     let yo={};
     yo.tieneComun=function(){
@@ -39,85 +39,4 @@ function Glizable(f,gl){
       }
     });
   };
-  */
-  let forma = f;
-  f.glizar=function(){
-    if(forma.glizada!=undefined && forma.glizada) return;
-
-    forma.glizada=true;
-
-    //generar los buffers de opengl
-
-    if(forma.position_buffer!=undefined){
-      let webgl_position_buffer = new GlPositionBuffer(gl).aPartirDe(forma.position_buffer);
-      forma.aVertexPosition=getter(webgl_position_buffer);
-    }
-    if(forma.normal_buffer!=undefined){
-      let glb = new GlNormalBuffer(gl).aPartirDe(forma.normal_buffer);
-      forma.aVertexNormal=getter(glb);
-    }
-    if(forma.color_buffer!=undefined){
-      let glb = new GlColorBuffer(gl).aPartirDe(forma.color_buffer);
-      forma.aVertexColor=getter(glb);
-    }
-    if(forma.texture_coord_buffer!=undefined){
-      let glb = new GlTextureCoordBuffer(gl).aPartirDe(forma.texture_coord_buffer);
-      forma.aTextureCoord=getter(glb);
-    }
-    if(forma.tangent_buffer!=undefined){
-      let glb = new GlNormalBuffer(gl).aPartirDe(forma.tangent_buffer);
-      forma.aVertexTangent=getter(glb);
-    }
-
-    if(forma.binormal_buffer!=undefined){
-      let glb = new GlNormalBuffer(gl).aPartirDe(forma.binormal_buffer);
-      forma.aVertexBinormal=getter(glb);
-    }
-    if(forma.index_buffer!=undefined){
-      let glb = new GlIndexBuffer(gl).aPartirDe(forma.index_buffer);
-      forma.getIndexBuffer=getter(glb);
-    }
-  }
 }
-
-/*
-function glizarForma(){
-  if(forma.glizada!=undefined && forma.glizada) return;
-
-  forma.glizada=true;
-
-  //generar los buffers de opengl
-
-  if(forma.position_buffer!=undefined){
-    let webgl_position_buffer = new GlPositionBuffer(gl).aPartirDe(forma.position_buffer);
-    forma.aVertexPosition=getter(webgl_position_buffer);
-  }
-  if(forma.normal_buffer!=undefined){
-    let glb = new GlNormalBuffer(gl).aPartirDe(forma.normal_buffer);
-    forma.aVertexNormal=getter(glb);
-  }
-  if(forma.color_buffer!=undefined){
-    let glb = new GlColorBuffer(gl).aPartirDe(forma.color_buffer);
-    forma.aVertexColor=getter(glb);
-  }
-  if(forma.texture_coord_buffer!=undefined){
-    let glb = new GlTextureCoordBuffer(gl).aPartirDe(forma.texture_coord_buffer);
-    forma.aTextureCoord=getter(glb);
-  }
-  if(forma.tangent_buffer!=undefined){
-    let glb = new GlNormalBuffer(gl).aPartirDe(forma.tangent_buffer);
-    forma.aVertexTangent=getter(glb);
-  }
-
-  if(forma.binormal_buffer!=undefined){
-    let glb = new GlNormalBuffer(gl).aPartirDe(forma.binormal_buffer);
-    forma.aVertexBinormal=getter(glb);
-  }
-  if(forma.index_buffer!=undefined){
-    let glb = new GlIndexBuffer(gl).aPartirDe(forma.index_buffer);
-    forma.getIndexBuffer=getter(glb);
-  }
-}
-
-glizarForma();
-*/
